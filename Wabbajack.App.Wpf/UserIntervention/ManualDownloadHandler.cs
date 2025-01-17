dcbe4271c70a56064ac81ad4990209ea9b85379a
Valiray <valirayyy@gmail.com>
2025-01-17 14:42:45 -0600

    auto downloader

--- Wabbajack.App.Wpf/UserIntervention/ManualDownloadHandler.cs
+++ Wabbajack.App.Wpf/UserIntervention/ManualDownloadHandler.cs
@@ -24,8 +24,39 @@ public class ManualDownloadHandler : BrowserWindowViewModel
             await RunJavaScript("Array.from(document.getElementsByTagName(\"iframe\")).forEach(f => {if (f.title != \"SP Consent Message\" && !f.src.includes(\"challenges.cloudflare.com\")) f.remove()})");
         });
         await NavigateTo(md.Url);
-        var uri = await task;
+        await RunJavaScript(@"
+        (function() {
+            var slowButton = document.getElementById('slowDownloadButton');
+            var chek = function(button) {
+                var PosT = {
+                        top: window.pageYOffset + button.getBoundingClientRect().top,
+                        left: window.pageXOffset + button.getBoundingClientRect().left,
+                        right: window.pageXOffset + button.getBoundingClientRect().right,
+                        bottom: window.pageYOffset + button.getBoundingClientRect().bottom
+                    },
+                    PosW = {
+                        top: window.pageYOffset,
+                        left: window.pageXOffset,
+                        right: window.pageXOffset + document.documentElement.clientWidth,
+                        bottom: window.pageYOffset + document.documentElement.clientHeight
+                    };
+                if (PosT.bottom > PosW.top &&
+                    PosT.top < PosW.bottom &&
+                    PosT.right > PosW.left &&
+                    PosT.left < PosW.right) {
+                    slowButton.click();
+                    setTimeout(function() {
+                        window.close();
+                    }, 2000);
+                }
+            };
+            window.addEventListener('scroll', function() {
+                chek(slowButton);
+            });
+            chek(slowButton);
+        })();");
 
+        var uri = await task;
         Intervention.Finish(uri);
         await Task.Delay(5000, CancellationToken.None);
     }
